6166d1e8310a15ffb8e1939bb08cd73c71a96702
Lasse Faurby Klausen <37332429+Faurby@users.noreply.github.com>
2021-12-10 12:33:07 +0100

    add ef database update to powershell (#26)

--- startup.ps1
+++ startup.ps1
@@ -1,15 +1,19 @@
 $project = "./MyApp/Server"
 $password = New-Guid
 
-Write-Host "Starting SQL Server"
+Write-Host "---- Starting SQL Server ----"
 docker rm --force StudyBankDB
 docker run --name StudyBankDB -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$password" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2019-latest 
 $database = "StudyBank"
 $connectionString = "Server=localhost;Database=$database;User Id=sa;Password=$password"
 
+Write-Host "---- Configuring User Secrets ----"
 Write-Host "Configuring Connection String"
 dotnet user-secrets init --project $project
 dotnet user-secrets set "ConnectionStrings:StudyBank" "$connectionString" --project $project
 
-Write-Host "Starting App"
+Write-Host "---- Migrating database ----"
+dotnet ef database update -p .\MyApp\Infrastructure\ -s .\MyApp\Server\ 
+
+Write-Host "---- Starting App ----"
 dotnet run --project $project
