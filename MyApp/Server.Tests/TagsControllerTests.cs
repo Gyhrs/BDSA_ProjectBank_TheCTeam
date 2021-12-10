afa2703934965b4e31b53e2fe9e9620d2db7b39f
Lasse Faurby Klausen <37332429+Faurby@users.noreply.github.com>
2021-12-10 15:30:14 +0100

    Add TagRepository, ITagReository and TagsController (#31)

---
+++ MyApp/Server.Tests/TagsControllerTests.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Moq;
+using MyApp.Server.Controllers;
+using MyApp.Shared;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Globalization;
+using System.Collections.Generic;
+
+public class TagsControllerTests
+{
+    [Fact]
+    public async Task Get_returns_Ok()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<TagsController>>();
+        var repository = new Mock<ITagRepository>();
+        repository.Setup(m => m.GetAllTags()).ReturnsAsync(Array.Empty<TagDTO>());
+        var controller = new TagsController(logger.Object, repository.Object);
+        // Act
+        var actual = await controller.GetAll();
+
+        // Assert
+        Assert.IsType<OkObjectResult>(actual.Result);
+    }
+}
