02eacd8c41421842014f3e33e2992e3b56fa3d31
Lasse Faurby <lasse_fk@hotmail.com>
2021-12-14 17:02:37 +0100

    deletede unnecessary method

--- MyApp/Infrastructure/Model/TagRepository.cs
+++ MyApp/Infrastructure/Model/TagRepository.cs
@@ -12,7 +12,6 @@ public class TagRepository : ITagRepository
     {
         _context = context;
     }
-
     public async Task<IReadOnlyCollection<TagDTO>> GetAllTags()
     {
         var tags = await (
@@ -23,32 +22,5 @@ public class TagRepository : ITagRepository
             )).ToListAsync();
 
         return tags.AsReadOnly();
-    }
-
-    public async Task<IReadOnlyCollection<ProjectDTO>> GetProjectsFromTags(List<string> searchTags)
-    {
-        var tags = await _context.Tags.Where(t => searchTags.Any(ttag => ttag == t.Name)).ToListAsync();
-
-        IEnumerable<Project> list = new List<Project>(tags.First().Projects);
-
-        for (int i = 1; i < tags.Count; i++)
-        {
-            list = list.Intersect(tags.ElementAt(i).Projects);
-        }
-
-        var projects = list.Select(p => new ProjectDTO(
-                            p.Id,
-                            p.Name,
-                            p.StartDate,
-                            p.EndDate,
-                            p.Description,
-                            p.Students != null ? p.Students.Select(s => s.Email).ToList() : null,
-                            p.Supervisors != null ? p.Supervisors.Select(s => s.Email).ToList() : null,
-                            p.CreatedBy != null ? p.CreatedBy.Email : null,
-                            p.CreatedBy != null ? p.CreatedBy.Name : null,
-                            p.Tags != null ? p.Tags.Select(t => t.Name).ToList() : null
-                        )).ToList();
-
-        return projects.AsReadOnly();
     }
 }
